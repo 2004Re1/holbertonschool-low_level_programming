fff2768900a599ded3909cb485aa6951cad3731c
2004Re1 <revanaxun559@gmail.com>
2024-02-19 14:20:42 +0000

    new tasl

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-int main (void)
-{
-char letter;
 
-for(letter = 'a' ; letter <= 'z' ; letter++ )
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-if(letter != 'q' || letter != 'e')
-{putchar(letter);}
-}
-putchar('\n');
-return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
