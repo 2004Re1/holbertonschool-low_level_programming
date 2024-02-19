7beac102cba02645ea477f09b458492319f50470
2004Re1 <revanaxun559@gmail.com>
2024-02-19 20:02:34 +0000

    new tasl

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch=", ";
+int num;
+
+for (num = 0 ; num < 10 ; num++)
+{
+putchar('0' + num);
+putchar(ch);
+}
+}
+putchar('\n');
+}
