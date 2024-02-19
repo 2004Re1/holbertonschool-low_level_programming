2b764c6e54eb4812d8b9f26452b2c288af1ad70a
2004Re1 <revanaxun559@gmail.com>
2024-02-19 20:06:03 +0000

    new tasl

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-char ch=", ";
+char ch = ", ";
 int num;
-
+char ch1 = "$";
 for (num = 0 ; num < 10 ; num++)
 {
 putchar('0' + num);
 putchar(ch);
 }
-}
+putchar(ch1);
 putchar('\n');
 }
