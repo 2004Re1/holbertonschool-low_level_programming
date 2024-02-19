8d1610c9125a3adda9632a98604a2986ac6615f1
2004Re1 <revanaxun559@gmail.com>
2024-02-19 11:36:30 +0000

    new tasl

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,9 +10,11 @@ char intType;
 int floatType;
 long int doubleType;
 long long int charType;
+float floattype;
 printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of double: %zu bytes\n", sizeof(doubleType));
 printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of char: %zu byte\n", sizeof(floattype));
 return (0);
 }
