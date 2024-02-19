01cfbe3c9887fdd5556bccbc5554e31e78899ebb
2004Re1 <revanaxun559@gmail.com>
2024-02-19 14:10:49 +0000

    new tasl

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+char letter;
+
+for(letter = 'a' ; letter <= 'z' ; letter++ )
+{
+if(letter != 'q' || letter != 'e'
+}
+putchar(letter)
+}
+}
+putchar('\n');
