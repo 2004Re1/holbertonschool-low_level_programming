3204f2e63c457632da650382fb482565853f47ec
2004Re1 <revanaxun559@gmail.com>
2024-02-20 08:56:05 +0000

    new tasl

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,8 +2,15 @@
 
 int print_last_digit(int a)
 {
-	int r; 
- 	r = a%10;
+	int r;
+       if(a < 0)
+       {	       
+ 	r = (-a)%10;
 	_putchar('0' + r);
+       }
+       else
+	r = (a)%10;
+        _putchar('0' + r);
+	
 	return (a%10);
 }
