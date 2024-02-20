87e7ef4aab33949f6eac8991c796f1a1f2266acb
2004Re1 <revanaxun559@gmail.com>
2024-02-20 08:01:09 +0000

    new tasl

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_sign - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int print_sign(int n);
+{
+	if(n > 0)
+		return(_purchar('+'));
+	if(n < 0)
+		return(_purchar('-'));
+	if(n == 0)
+		return(_purchar('0'));
+
+}
