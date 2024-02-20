40ee1a3af20d47bbf5d65e70728a8e38e1a4ae97
2004Re1 <revanaxun559@gmail.com>
2024-02-20 09:02:49 +0000

    new tasl

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,18 @@
 #include "main.h"
 
-int print_last_digit(int a)
+/**
+*print_last_digit - Fuck Betty
+*@n: parameter description(frick you betty)
+*Return: I don't know and I don't even want to.
+*/
+int print_last_digit(int n)
 {
-	int r;
-	r = a < 0 ? (-1*a)%10 : a%10;
-	_putchar (r + '0');
-	return (a < 0 ? (-1*a)%10 : a%10);
+
+int abs;
+int d;
+
+d = n % 10;
+abs = d < 0 ? d * -1 : d;
+_putchar('0' + abs);
+return (abs);
 }
