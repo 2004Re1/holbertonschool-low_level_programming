f4cc3b4ebbcd72ab40d7f3417be2af5a8aff6c78
2004Re1 <revanaxun559@gmail.com>
2024-02-20 07:53:43 +0000

    new tasl

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_islower - controls if a character is in lowercase
+*@c: character to be verified
+*Return: return 0 or 1
+*/
+int _isalpha(int c)
+{
+return (1 ? (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') : 0)
+}
