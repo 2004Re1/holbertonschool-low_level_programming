904c4d965acfb59187cb5a561cd2778717b3e797
2004Re1 <revanaxun559@gmail.com>
2024-02-20 10:11:22 +0000

    new tasl

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -33,17 +33,13 @@ void print_to_98(int n)
 
 void print(long n)
 {
-    // If number is smaller than 0, put a - sign
-    // and change number to positive
     if (n < 0) {
         putchar('-');
         n = -n;
     }
 
-    // Remove the last digit and recur
     if (n/10)
         print(n/10);
 
-    // Print the last digit
     putchar(n%10 + '0');
 }
