3a9cefd124090c4523ea3c5ab84c99eab8057d47
2004Re1 <revanaxun559@gmail.com>
2024-02-20 08:10:52 +0000

    new tasl

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,18 +1,25 @@
 #include "main.h"
+
 /**
-*print_sign - controls if a character is in lowercase
-*@c: character to be verified
-*Return: return 0 or 1
+*print_sign - returns the sign of a number
+*@n: number to be checked
+*Return: -1, 1 or 0
 */
 int print_sign(int n)
 {
-	if(n > 0)
-		_putchar('+')
-		return(1);
-	if(n < 0)
-		_putchar('-')
-		return(-1);
-	if(n == 0)
-		_putchar('0')
-		return(0);
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
