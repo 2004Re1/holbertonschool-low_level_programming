5c669c03df0cd74011f0f34aa07d0e52c7ec6785
2004Re1 <revanaxun559@gmail.com>
2024-02-20 07:31:26 +0000

    new tasl

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
+
 /**
- * main - Entry point
+ * print_alphabet - prints the alphabet
  *
  * Return: Always 0 (Success)
+ *
  */
-
 void print_alphabet(void)
 {
-char c;
-for( c = 'a' ; c <= 'z' ; c++)
-_putchar('c');
-_putchar('\n');
-}
+char letter;
 
+for (letter = 'a'; letter <= 'z'; letter++)
+	_putchar(letter);
+_putchar('\n');
 }
