55ffb8f9d107da70582d24695939da3654cd2db2
2004Re1 <revanaxun559@gmail.com>
2024-02-20 09:42:21 +0000

    new tasl

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <98)
+		{
+		int r;
+		r = n;
+
+		while (r >= 10)
+		       r = r / 10;
+		_putchar('0' + r)	
+
+		_putchar('0' + n%10)
+
+		n++;
+		}
+
+
+	}
+	
+	if (n > 98)
+        {
+                while (n > 98)
+                {
+                int r;
+                r = n;
+
+                while (r >= 10)
+                       r = r / 10;
+                _putchar ('0' + r)
+
+                _putchar ('0' + n%10)
+
+                n--;
+                }
+
+
+        }
+
+	if (n == 98)
+		_putchar ('0' + n)
+	
+}
