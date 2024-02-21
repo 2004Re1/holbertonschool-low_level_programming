7bfcf1f1ab2c0e16f4a1070178d374177a1d8d60
2004Re1 <revanaxun559@gmail.com>
2024-02-21 01:22:01 -0800

    new tasl

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int n;
+	for(n = 0 ; n <24 ; n++)
+	{
+		if(n <10)
+		{	
+			print(0);
+			print(n);
+		}
+		else
+		print(n);
+		_putchar(':');
+
+		for(n = 0 ; n < 60 ; n++)
+			{
+				if(n <10)
+                		{
+                        		print(0);
+                        		print(n);
+           			     }
+				else
+				print(n);
+                		_putchar(':');
+			}
+		
+	}	
+}
+
+
+void print(int n) {
+    
+    if (n < 0) {
+        _putchar('-');
+        n = abs(n);
+    }
+    
+    if (n > 0) 
+        print(n / 10);
+    else return;
+    
+    _putchar('0' + n % 10);
+}
