7678940ecfef605f6d82ea186d9fd4220b9ebb7f
2004Re1 <revanaxun559@gmail.com>
2024-02-21 01:47:21 -0800

    new tasl

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,45 +1,52 @@
 #include "main.h"
-void print(int n);
-void jack_bauer(void)
-{
-	int n;
-	for(n = 0 ; n <24 ; n++)
-	{
-		if(n <10)
-		{	
-			print(0);
-			print(n);
-		}
-		else
-		print(n);
-		_putchar(':');
-
-		for(n = 0 ; n < 60 ; n++)
-			{
-				if(n <10)
-                		{
-                        		print(0);
-                        		print(n);
-           			     }
-				else
-				print(n);
-                		_putchar(':');
-			}
-		
-	}	
-}
-
-
+/**
+*print - Fsjkoipfhasiofh
+*@n: doinf something
+*/
 void print(int n) {
     
     if (n < 0) {
-        _putchar('-');
-        n = -1*n;
+        putchar('-');
+        n = abs(n);
     }
     
     if (n > 0) 
         print(n / 10);
     else return;
     
-    _putchar('0' + n % 10);
+    putchar('0' + n % 10);
+}
+/**
+*jack_bauer - Fuck Betty
+*/
+void jack_bauer(void) 
+{
+    // Write C code here
+    
+    int i,j;
+    for(i = 0 ; i < 24 ; i++)
+    {
+        for(j = 0 ; j < 60 ; j++)
+        {
+            if(i == 0)
+            putchar('0');
+            if(i<10)
+            putchar('0');
+            print(i);
+            
+            putchar(':');
+            
+            
+            if(j == 0)
+            putchar('0');
+            if(j<10)
+            putchar('0');
+            print(j);
+            
+            putchar('\n');
+
+        }
+    }
+
+    return 0; 
 }
