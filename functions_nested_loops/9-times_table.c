c615b6bfb65e0e9d6beb132cdd483472b362a9b1
2004Re1 <revanaxun559@gmail.com>
2024-02-22 21:27:49 +0000

    new tasl

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
-/*
+/**
  * sign - return
  * @n: something
  * return: -1 1 or 0
- * */
+ */
 int sign(int n)
 {
 	if (n < 0)
-			return (-1);
+		return (-1);
 	else if (n > 0)
-			return (1);
+		return (1);
 	else
-			return (0);
+		return (0);
 }
 
-/*
+/**
  * abs - return
  * @n: something
  * return: -1 1 or 0
@@ -36,7 +36,7 @@ void print_num(int t)
 	int dec;
 
 	if (t < 0)
-			_putchar('-');
+		_putchar('-');
 	t = abs(t);
 
 	num = t;
