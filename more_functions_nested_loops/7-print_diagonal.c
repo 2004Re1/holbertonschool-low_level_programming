93eb2a2232b71045b9a4f36be2275b71949b0b71
2004Re1 <revanaxun559@gmail.com>
2024-02-27 10:35:40 +0000

    new tasl

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * *print_line - returns the sign of a number
+ * *print_diagona- returns the sign of a number
  * *@n: number to be checked
  * *Return: -1, 1 or 0
  */
-void print_line(int n)
+void print_diagonal(int n)
 {
         int i;
 for (i = 0; i < n ; i++)
