f402b420d2af11a69d7f0bc1c2eb10de8969fbc8
2004Re1 <revanaxun559@gmail.com>
2024-02-27 09:35:59 +0000

    new tasl

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *mul - returns the absolute value of a number
-*@a @b: number to be checked
+*@a: @b: number to be checked
 *Return: the absolute value
 */
 int mul(int a, int b)
