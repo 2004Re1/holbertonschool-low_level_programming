1d52c85c4712b073a9e41beda880601651dc04e9
2004Re1 <revanaxun559@gmail.com>
2024-02-27 09:47:06 +0000

    new tasl

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,18 @@
-#include "math.h"
+#include "main.h"
+
+/**
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
 void print_numbers(void)
 {
-	int i;
-	for(i = 0 ; i < 10 ; i++)
-{
-	_putchar('0'+i);
+	char x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		_putchar(x);
+	}
+
 	_putchar('\n');
-}
 }
