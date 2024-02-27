2984a78517ccc35ca10eed24156ffb6f4e23895a
2004Re1 <revanaxun559@gmail.com>
2024-02-27 10:28:41 +0000

    new tasl

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *print_line - returns the sign of a number
+ * *@n: number to be checked
+ * *Return: -1, 1 or 0
+ */
+void print_line(int n)
+{
+        int i;
+for (i = 0; i < n ; i++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
