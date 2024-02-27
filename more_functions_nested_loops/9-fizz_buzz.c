3ff35e78012171496e18c39d157dd898217b9baa
2004Re1 <revanaxun559@gmail.com>
2024-02-27 11:53:18 +0000

    new tasl

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,45 +1,37 @@
 #include <stdio.h>
 #include "main.h"
-void print(int n)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	if (n/10)
-	print(n/10);
-	putchar(n%10 + '0');
-}
+	int i;
 
-int main (void)
-{
-
-int i;
-for (i = 1 ; i < 100 ; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-{
-putchar('F');
-putchar('i');
-putchar('z');
-putchar('z');
-putchar('B');
-putchar('u');
-putchar('z');
-putchar('z');        
-}
-else if (i % 3 == 0)
-{
-putchar('F');
-putchar('i');
-putchar('z');
-putchar('z');       
-}
-else if (i % 5 == 0)
-{
-putchar('B');
-putchar('u');
-putchar('z');
-putchar('z');
-}
-else
-print(i);
-putchar(' ');        
-}
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
