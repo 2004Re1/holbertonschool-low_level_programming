b2cbdf8c7c147a830d8bcd56a703deaa5ed2b9aa
2004Re1 <revanaxun559@gmail.com>
2024-02-27 11:43:24 +0000

    new tasl

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+void print(int n)
+{
+	if (n/10)
+	print(n/10);
+	putchar(n%10 + '0');
+}
+
+int main (void)
+{
+
+int i;
+for (i = 1 ; i < 100 ; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+putchar('F');
+putchar('i');
+putchar('z');
+putchar('z');
+putchar('B');
+putchar('u');
+putchar('z');
+putchar('z');        
+}
+else if (i % 3 == 0)
+{
+putchar('F');
+putchar('i');
+putchar('z');
+putchar('z');       
+}
+else if (i % 5 == 0)
+{
+putchar('B');
+putchar('u');
+putchar('z');
+putchar('z');
+}
+else
+print(i);
+putchar(' ');        
+}
+}
+
