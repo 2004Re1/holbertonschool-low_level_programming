17605ae879d69dd8b4f6241fa25689357fce98dd
2004Re1 <revanaxun559@gmail.com>
2024-02-27 09:25:59 +0000

    new tasl

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,13 @@
 #include "main.h"
-
+/**
+*_isdigit - returns the absolute value of a number
+*@c: number to be checked
+*Return: the absolute value
+*/
 int _isdigit(int c)
 {
 	if (c >= 0 && c <= 9)
-		return (1);
-	else
 		return (0);
+	else
+		return (1);
 }
