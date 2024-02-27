827e5ec61e3bbfe1ecdd741b2fe974d301ddf4c0
2004Re1 <revanaxun559@gmail.com>
2024-02-27 11:25:15 +0000

    new tasl

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *print_diagonal- returns the sign of a number
+ * *@n: number to be checked
+ * *Return: -1, 1 or 0
+ */
+void print_square(int n)
+{
+int i, j;
+for (i = 0 ; i < n ; i++)
+{
+	for (j = 0 ; j < n ; j++)
+	_putchar(35);
+	_putchar('\n');
+}
+_putchar('\n');
+}
