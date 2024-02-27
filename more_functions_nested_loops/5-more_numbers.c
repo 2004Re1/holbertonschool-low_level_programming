d9611a4a1fbb9c7b02a9b5c0de752ba24c0b16b9
2004Re1 <revanaxun559@gmail.com>
2024-02-27 10:13:33 +0000

    new tasl

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,25 @@
 #include "main.h"
 /**
  * more_numbers - prints the alphabet
- *
+ * print - printing
  * Return: Always 0 (Success)
  *
  */
+void print(int n)
+}
+	if (n/10)
+		print(n/10);
+	putchar(n%10 + '0');
+}
 void more_numbers(void)
 {
 	int i;
-	char j;
+	int j;
 for(i = 0 ; i < 10 ; i++)
 {
-for(j = '0' ; j < '15' ; j++)
+for(j = 0 ; j < 15 ; j++)
 {
-_putchar('0' + j);
+print(j);
 }
 _putchar('\n');
 }
