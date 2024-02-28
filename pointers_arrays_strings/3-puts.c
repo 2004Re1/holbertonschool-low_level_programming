cb5406faf3903877e0c7d5747acc39398fa32d36
2004Re1 <revanaxun559@gmail.com>
2024-02-28 09:59:42 +0000

    new tasl

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry point
+ *
+ * @s: variable
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	while(*p != '\0')
+	{
+		puts(str);
+	}
+}
