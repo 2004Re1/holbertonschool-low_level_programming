606acc2be86d392a89654133b93dabf2ae306bb5
2004Re1 <revanaxun559@gmail.com>
2024-02-28 09:20:19 +0000

    new tasl

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * *swap_int returns the sign of a number
+ * *@b: number to be checked
+ * *@a: akshdfioa
+ * *Return: -1, 1 or 0
+ */
+void swap_int(int *a, int *b)
+{
+	int p;
+	p = *a;
+	*a = *b;
+	*b = p;
+}
