6891134b11a8d7e281741205d8c9f1d3db2f0d93
2004Re1 <revanaxun559@gmail.com>
2024-02-29 17:47:56 +0000

    new tasl

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,23 +3,23 @@
 #include <stdio.h>
 
 /**
- * _puts - Entry point
+ * print_rev - Entry point
  *
- * @str: variable
+ * @s: variable
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
 	char *help;
-	help = s;
-	while(*help != '\0')
+help = s;
+	while (*help != '\0')
 		help++;
 	help--;
 
-	while(help >= s)
+	while (help >= s)
 	{
 	       _putchar(*help);
-		help--;	
+		help--;
 	}
 	_putchar('\n');
 }
