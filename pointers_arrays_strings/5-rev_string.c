0a0de05a80fb77bbd1283b4b3515fdd6d10bd614
2004Re1 <revanaxun559@gmail.com>
2024-03-01 11:02:19 +0000

    new tasl

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,28 @@
-#include <string.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rev_string - Entry point
- *
- * @s: variable
- * Return: Always 0.
+ * rev_string - reverses a string
+ * @s: string to be reversed
  */
 void rev_string(char *s)
 {
-        char *help;
-help = s;
-        while (*help != '\0')
-                help++;
-        help--;
-while (help >= s)
-{
-_putchar(*help);
-                help--;
-}
-        _putchar('\n');
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
