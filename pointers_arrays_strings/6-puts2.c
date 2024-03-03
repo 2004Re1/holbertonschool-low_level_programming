7feaa0a594890151cc5e3189179083f9c3e735fa
2004Re1 <revanaxun559@gmail.com>
2024-03-03 09:11:29 +0000

    new tasl

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "main.h"
-/*
+/**
  * _strlen - skadjafa
  * @s: akjdpajfa
  * return - nese
- * */
-int _strlen(char* s)
+ */
+int _strlen(char *s)
 {
 	char *p = s;
 
@@ -13,16 +13,16 @@ int _strlen(char* s)
 		p++;
 	return (p - s);
 }
-/*
+/**
  * puts2 - mkaSKDAFD
  * @str: sakdakfal
- * */
+ */
 
 void puts2(char *str)
 {
 	int n = _strlen(str);
 	int i;
-	for (i = 0 ; i < n ; i++)
+for (i = 0 ; i < n ; i++)
 	if (i % 2 == 0)
 	_putchar(*(str + i));
 	_putchar('\n');
