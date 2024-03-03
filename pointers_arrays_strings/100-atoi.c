b11e4f6eeecd50ec845a6cb615a452989f0ad83d
2004Re1 <revanaxun559@gmail.com>
2024-03-03 10:32:31 +0000

    new tasl

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,24 +1,35 @@
-#include <stdio.h> 
-#include <string.h> 
+#include "main.h"
+#include <stdio.h>
 /**
- *_atoi - Entry point
- * @s: 'var'
- * Return: Always 0 (Success)
+ * _atoi - check the code
+ * @s: pointer
+ * Return: Always 0.
  */
 int _atoi(char *s)
-{ 
-	int num = 0;
-	int i = 0;
-	for (i = 0 ; s[i] != '\0' ; i++) 
-	{
-	if (s[i] >=48 && s[i] <= 57)	
-		num = num * 10 + (s[i] - 48); 
-	}
-	for(i = 0 ; s[i] != '\0' ; i++)
-	{
-	if (s[i] == 45)
-	num = -num;	
-	}
-	return (num); 
-}
+{
+	int i, m = 0;
+	unsigned int n = 0;
 
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		if (*(s + i) == 45)
+		{
+			m++;
+		}
+		if (*(s + i) >= 48 && *(s + i) <= 57)
+		{
+			n = n * 10 + (*(s + i) - 48);
+
+			if (*(s + i + 1) < 48 || *(s + i + 1) > 57)
+			{
+				break;
+			}
+		}
+	}
+	if (m % 2 == 1)
+	{
+		n = -n;
+	}
+
+	return (n);
+}
