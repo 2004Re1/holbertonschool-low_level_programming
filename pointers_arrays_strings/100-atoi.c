93614f982dba773b128cb0e80b1b6edfd53ca0b9
2004Re1 <revanaxun559@gmail.com>
2024-03-03 10:29:11 +0000

    new tasl

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,7 +13,12 @@ int _atoi(char *s)
 	{
 	if (s[i] >=48 && s[i] <= 57)	
 		num = num * 10 + (s[i] - 48); 
-	}  
+	}
+	for(i = 0 ; s[i] != '\0' ; i++)
+	{
+	if (s[i] == 45)
+	num = -num;	
+	}
 	return (num); 
 }
 
