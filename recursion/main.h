15b4cddbeb883b6c3ce8d83aeadf904ca3344f2e
2004Re1 <revanaxun559@gmail.com>
2024-03-07 12:41:39 +0000

    new tasl

--- recursion/main.h
+++ recursion/main.h
@@ -20,4 +20,9 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
 #endif
