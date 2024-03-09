b6671ace402b2a20addeece66c767af651db0bd8
2004Re1 <revanaxun559@gmail.com>
2024-03-09 10:36:02 +0000

    new tasl

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
-int main (int argc ,char *argv[])
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
 {
-	(void) arcg;
-	printf("%s\n",argv[0]);
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+
 	return (0);
 }
