35e034f8d4f3f0acdd598a87f8daf728d7597baa
2004Re1 <revanaxun559@gmail.com>
2024-03-09 10:45:32 +0000

    new tasl

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * @argc: var
+ * @argv: var
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	for(i = 0 ; i < argc ; i++)
+		printf("%s\n", argv[i]);
+
+
+	return (0);
+}
