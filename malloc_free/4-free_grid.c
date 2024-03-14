3dd16ed159e143a27e5a9fae35e675029a230c80
2004Re1 <revanaxun559@gmail.com>
2024-03-14 10:36:26 +0000

    new tasl

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,94 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-
 /**
- * print_grid - prints a grid of integers
- * @grid: the address of the two dimensional grid
- * @width: width of the grid
- * @height: height of the grid
- *
- * Return: Nothing.
+ * free_grid - frees a 2D grid previously created
+ * @grid: input pointer to grid or 2D array
+ * @height: height of 2D arrray
+ * Return: void
  */
- 
- int **alloc_grid(int width, int height)
-{
-	int **arr;
-	int i, j;
-
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
-	arr = malloc(sizeof(int *) * height);
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < height; i++)
-	{
-		arr[i] = malloc(sizeof(int) * width);
-		if (arr[i] == NULL)
-		{
-			for (i--; i >= 0; i--)
-				free(arr[i]);
-			free(arr);
-			return (NULL);
-		}
-		for (j = 0; j < width; j++)
-		{
-			arr[i][j] = 0;
-		}
-	}
-	return (arr);
-}
- 
 void free_grid(int **grid, int height)
 {
-    free(grid);
-    
-    for(int i = 0 ; i < height ; i++)
-        free(grid[i]);
-    
-}
- 
-void print_grid(int **grid, int width, int height)
-{
-    int w;
-    int h;
+	int i;
 
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int **grid;
-
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    free_grid(grid, 4);
-    return (0);
+	if (grid == NULL)
+		return;
+	for (i = height; i >= 0; i--)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
