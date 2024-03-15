40bc73491d3abb282e192ea7dc426f587deaeeec
2004Re1 <revanaxun559@gmail.com>
2024-03-15 17:50:50 +0000

    new tasl

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+/**
+ *  *_calloc - check the code
+ *   *
+ *    *@nmemb: var
+ *     *@size: var
+ *      *
+ *       *Return: Always 0.
+ *        */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+		if (size == NULL || nmemb == NULL)
+					return (NULL);
+
+			char *ptr;
+				ptr = malloc(nmemb *size);
+
+					if (ptr == NULL)
+								return (NULL);
+
+						int i;
+							for (i = 0; i < nmemb * size; i++)
+										ptr[i] = 0;
+
+								return (ptr);
+
+									return ptr;
+}
