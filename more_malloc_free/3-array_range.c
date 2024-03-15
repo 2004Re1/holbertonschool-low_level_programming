2c7ce749ea2c369c5cc9f2cd0c15d24a8e3a1639
2004Re1 <revanaxun559@gmail.com>
2024-03-15 18:31:58 +0000

    new tasl

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * array_range - check the code
+ *
+ * @min: var
+ * @max: var
+ *
+ * Return: Always 0.
+ */
+int *array_range(int min, int max)
+{
+	int *a;
+	int i;
+	int n = max - min;
+
+	if (min > max)
+		return (NULL);
+
+	a = malloc(sizeof(int) *n);
+
+	for (i = 0; i < n; i++)
+		a[i] = i;
+
+	if (a == NULL)
+		return (NULL);
+
+	return (a);
+}
