e65cd5e218bd397551d66472192d01f1d3dc2a4c
2004Re1 <revanaxun559@gmail.com>
2024-03-22 17:14:08 +0000

    new tasl

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,10 +1,14 @@
 /**
- * dog - is a struct
- * */
-
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
