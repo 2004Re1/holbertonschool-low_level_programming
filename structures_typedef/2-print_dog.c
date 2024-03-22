bbf813dd64a6bed09d3fe72005ca792932afeb32
2004Re1 <revanaxun559@gmail.com>
2024-03-22 17:35:16 +0000

    new tasl

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,13 +5,13 @@
   * print_dog - initialize struct dog.
   * @d: name of struct
   */
-
 void print_dog(struct dog *d)
 {
-if (d)
-{
-	printf("Name: %s\n",d->name);
-	printf("Age: %d\n",d->age);
-	printf("Owner: %s\n",d->owner);
-}
+	if (d == NULL)
+	{
+		return;
+	}
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
