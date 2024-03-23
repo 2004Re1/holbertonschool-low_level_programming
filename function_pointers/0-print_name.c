bbfa292ffe9ba16ad6c7c0bf1793fbe498d982eb
2004Re1 <revanaxun559@gmail.com>
2024-03-23 11:23:04 +0000

    new tasl

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f:f
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void f(name);
+}
