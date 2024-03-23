7525a4764d040f4ff65574d44e4e7b3e26e329f1
2004Re1 <revanaxun559@gmail.com>
2024-03-23 11:40:46 +0000

    new tasl

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints an integer.
+ * @array: the integer to point
+ * @action:var
+ * @size: var
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	
+	for(i = 0 ; i < size ; i++)
+		action(array[i]);
+}
