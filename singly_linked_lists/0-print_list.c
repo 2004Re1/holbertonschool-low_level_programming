f7e362d2e01e9385fb1b9fb576dacc02f9e8d0f6
2004Re1 <revanaxun559@gmail.com>
2024-04-01 10:35:00 +0000

    new tasl

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,21 +11,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count;
+	size_t count = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("%s", h->str);
-			h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
 		}
+		h = h->next;
 		count++;
-
 	}
 	return (count);
-
 }
