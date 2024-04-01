37a175d0f4fce47a1723dd9646849e4b882f014a
2004Re1 <revanaxun559@gmail.com>
2024-04-01 20:09:28 +0000

    new tasl

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,49 +4,41 @@
 #include "lists.h"
 
 /**
- * _strlen - Entry point
- *
- * @s: variable
- * Return: Always 0.
+ * _strlen - Returns the length of a string.
+ * @s: The string to check.
+ * Return: Length of the string.
  */
 int _strlen(const char *s)
 {
-	char *p = s;
+	int i;
 
-	while (*p != '\0')
-		p++;
-	return (p - s);
+	i = 0;
+	while (s[i] != 0)
+		i++;
+
+	return (i);
 }
 
 /**
- * add_node_end - check the code
+ * add_node - check the code
  *
  * @head: var
  * @str: var
  *
  * Return: Always 0.
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *last;
-	list_t *new_n = malloc(sizeof(list_t));
+	list_t *new_n;
 
+	new_n = malloc(sizeof(list_t));
 	if (new_n == NULL)
 		return (NULL);
 
 	new_n->str = strdup(str);
 	new_n->len = _strlen(str);
-	new_n->next = NULL;
-
-	if (*head == NULL)
-		*head = new_n;
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new_n;
-	}
+	new_n->next = *head;
+	*head = new_n;
 
 	return (new_n);
 }
