913609c304b5f5944095b29c3c4474943ce8b7bf
2004Re1 <revanaxun559@gmail.com>
2024-04-01 10:27:31 +0000

    new tasl

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-
+#include <string.h>
+/**
+ * print_list - prints each element of linked list with lengths.
+ *
+ * @h: struct that provides string, length and address of next node.
+ *
+ * Return: the number of nodes in given linked list.
+ */
 size_t print_list(const list_t *h);
 {
 	while (h != NULL)
