0b4fd60169e57c5e35be13d13b5e1489d417e4da
2004Re1 <revanaxun559@gmail.com>
2024-04-01 20:04:32 +0000

    new tasl

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -35,7 +35,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_n->str = strdup(str);
-	new_n->len = strlen(str);
+	new_n->len = _strlen(str);
 	new_n->next = *head;
 	*head = new_n;
 
