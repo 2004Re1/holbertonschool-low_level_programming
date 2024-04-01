74b633fc73c47ed6bb0cebb8ed6e376293d7e9cc
2004Re1 <revanaxun559@gmail.com>
2024-04-01 10:23:00 +0000

    new tasl

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+void printLinkedlist(struct node *p)
+{
+	while (p != NULL)
+	{
+		if (p->str == NULL)
+		{
+			printf()
+		}
+		else
+		{
+			printf("%d ", p->str);
+			p = p->next;
+		}
+	}
+}
