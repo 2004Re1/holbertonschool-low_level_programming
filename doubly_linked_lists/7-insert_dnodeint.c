ec593d681485b8c1c8ce30f74d31bd7ca2dbfa5a
2004Re1 <revanaxun559@gmail.com>
2024-04-06 15:58:43 +0000

    new tasl

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * *insert_dnodeint_at_index - check the code
+ *
+ * @h: var
+ * @idx: var
+ * @n: var
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+dlistint_t* insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t * node1;
+	node1 = malloc(sizeof(dlistint_t));
+	node1->n = n;
+
+	dlistint_t *temp, *temp1;
+	temp = *h;
+	temp1 = (*h)->next;
+	idx--;
+	for (int i = 0; i < idx; i++)
+	{
+		temp = temp->next;
+		temp1 = temp->next;
+	}
+
+	temp->next = node1;
+	node1->prev = temp;
+	temp1->prev = node1;
+	node1->next = temp1;
+
+	return (*h);
+
+}
