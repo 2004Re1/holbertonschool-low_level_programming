d084d161f28bea75d89a74b4d1928cafa785563c
2004Re1 <revanaxun559@gmail.com>
2024-04-06 13:25:07 +0000

    new tasl

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *free_dlistint - check the code
+ *
+ *@head: var
+ *
+ *Return: Always EXIT_SUCCESS.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t * temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+
+	free(head);
+}
